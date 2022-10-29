8e185a858db500cfb2a4430fc13830296f1406a2
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-30 01:28:40 +0300

    post data to backend with fetch api is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name, email)
+
+    const user = { name, email }
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        const newUsers = [...users, data]
+        setUsers(newUsers)
+
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" placeholder='Your name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='Your Email' />
+        <br />
+        <button>Add new user</button>
+      </form>
+      <div>
+        {
+          users.map(u => <p key={u.id}>{u.name}:{u.email}</p>)
+        }
+      </div>
     </div>
   );
 }
